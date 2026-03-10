188e0c9cb035a2f38ec1830f87dd79bce31407f8
Martin John M <johnmmartin74@gmail.com>
2026-03-10 12:22:49 +0100

    Created graceful shutdown of the server

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::{sync::{Arc, Mutex, mpsc}, thread};
 
 pub struct ThreadPool {
     workers: Vec<(Worker)>,
-    sender : mpsc::Sender<Job>
+    sender : Option<mpsc::Sender<Job>>
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
@@ -31,7 +31,10 @@ impl ThreadPool {
             // Create some threads and store them in the vector
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
-        ThreadPool { workers, sender }
+        ThreadPool { 
+            workers, 
+            sender : Some(sender)
+        }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -40,22 +43,44 @@ impl ThreadPool {
     {
         let job = Box::new(f);
 
-        self.sender.send(job).unwrap();
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+
+        // drop the sender first
+        drop(self.sender.take());
+
+        for worker in self.workers.drain(..) {
+            println!("shutting down workers {}", worker.id);
+
+            worker.thread.join().unwrap();
+        }
     }
 }
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<Arc<std::sync::Mutex<std::sync::mpsc::Receiver<Job>>>>,
+    thread: thread::JoinHandle<()>,
 }
 
 impl Worker {
     fn new (id : usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || {
             loop {
-                let job = receiver.lock().unwrap().recv().unwrap();
-                println!("Worker {id} got a job; executing.");
-                job();
+                let message = receiver.lock().unwrap().recv();
+                match message {
+                    Ok(job) => {
+                        println!("Worker {id} got a job; executing");
+                        job();
+                    }
+                    Err(_) => {
+                        println!("Worker {id} disconnected; shutting down");
+                        break;
+                    }
+                }
             }
         });
 
